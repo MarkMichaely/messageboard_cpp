4d16f785a3a32342e4330837e367ed8111b5e5d3
MarkMichaely <44968872+MarkMichaely@users.noreply.github.com>
2021-04-07 12:06:12 +0300

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -12,18 +12,57 @@
 using ariel::Direction;
 
 #include <iostream>
-#include <stdexcept>
 using namespace std;
 
 namespace ariel{
-    void Board::post (uint row, uint column, Direction direction, std::string messsage) {
-
+    void Board::post (uint row, uint column, Direction direction, std::string const &messsage) {
+        this->min_row = min(this->min_row,row);
+        this->max_row = max(this->max_row,row);
+        this->min_column = min(this->min_column,column);
+        this->max_column = max(this->max_column,column);
+        for (char c: messsage){
+            this->board[row][column].def = c;
+            if (direction == Direction::Horizontal) {
+                column++;
+            }
+            else{
+                row++;
+            }
+        }
     }
     std::string Board::read (uint row, uint column, Direction direction, uint length){
-        return "blabla";
+        std::string ans;
+        for (size_t i = 0; i < length; i++)
+        {
+            ans += this->board[row][column].def;
+            if (direction == Direction::Horizontal) {
+                column++;
+            }
+            else{
+                row++;
+            }
+        }
+        
+        return ans;
     }
     void Board::show(){
-        std::cout<< "bla"<<std::endl;
+        cout << "|";
+        for (size_t i = 0; i <= this->max_row-this->min_row; i++){
+            cout << "=";
+        }
+        cout << "|\n";
+        for (size_t i = this->min_row; i <= this->max_row; i++) {
+            cout << "|";
+            for (size_t j = this->min_column; j <= this->max_column; j++) {
+                cout << this->board[i][j].def;
+            }
+            cout << "|\n";
+            
     }
+        cout << "|";
+        for (size_t i = 0; i <= this->max_row-this->min_row; i++){
+            cout << "=";
+        }
+        cout << "|\n";    
 }
-
+}
--- Board.hpp
+++ Board.hpp
@@ -1,7 +1,22 @@
 #pragma once
+/**
+ * h file for message-board exercise.
+ * 
+ * sources used:
+ * https://stackoverflow.com/questions/2333728/stdmap-default-value
+ * 
+ * Author: Mark Michaely
+ * Since : 2021-03
+ */
+
+
 #include "Direction.hpp"
 #include <string>
-#include <vector>
+#include <map>
+
+struct makeEmptyValuesToUnderscore{
+    char def = '_';
+};
 
 using ariel::Direction;
 using namespace std;
@@ -9,16 +24,20 @@ using namespace std;
 namespace ariel {
     class Board {
         private:
-            std::vector<std::vector<std::string>> board;
+            uint min_row, max_row, min_column, max_column;
+            std::map<uint, std::map<uint, makeEmptyValuesToUnderscore>> board;
         
         public:
             Board() {
-
+                max_row=0;
+                max_column=0;
+                min_column=INT8_MAX;
+                min_row=INT8_MAX;
             }
             ~Board(){
 
             }
-            void post (uint row, uint column, Direction direction, std::string messsage);
+            void post (uint row, uint column, Direction direction, std::string const &messsage);
             std::string read (uint row, uint column, Direction direction, uint length);
             void show();
     };
